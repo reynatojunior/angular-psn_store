243a564c0c583f49782e99441b72a639b30aaf37
reynatojunior <reynato.junior@hotmail.com>
2024-03-19 16:56:53 -0300

    projeto final

---
+++ src/app/components/menu-bar/menu-bar-item/menu-bar-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar-item',
+  templateUrl: './menu-bar-item.component.html',
+  styleUrl: './menu-bar-item.component.css'
+})
+export class MenuBarItemComponent {
+
+}
